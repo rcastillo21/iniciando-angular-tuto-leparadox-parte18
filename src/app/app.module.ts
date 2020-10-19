ce82039594fa54b52188ac3fb44922a83a2eb980
MSCINSYSTEMS-PC\MSC IN SYSTEMS <Ruben>
2020-10-19 11:35:26 -0430

    Creacion de Componentes y Configuracion Basica, se creo Pipe para realizar comparacion en la busqueda, se trajo servicio de la Web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './components/app/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchComponent } from './components/search/search.component';
+
+/*Angular Material*/
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { SearchPipe } from './pipes/search.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'parte18';
+
+  handleSearch(value: string){
+    //console.log(value);
+    this.filtroValor = value;
+  }
+
+  filtroValor = '';
+  constructor(
+    private _http: HttpClient
+  ) {
+
+  }
+  ngOnInit(){
+    this._http.get('https://jsonplaceholder.typicode.com/users').subscribe((users: any[]) => this.usuarios = users);
+  }
+
+  usuarios = [];
 }
